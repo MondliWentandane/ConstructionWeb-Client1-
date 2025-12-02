abe610080f707ccd9fa16f2b84d71907c66fcbd2
Mondli <mondli46401@gmail.com>
2025-12-02 11:25:35 +0200

    Here I am trying to do the about section(Mondli Khoza)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import navLog from '../assets/Logo.png'
+import navLog from '../assets/Logo1.png'
 
 const Navbar: React.FC = () => {
   return (
     <nav className="main-navbar">
-        <img src={navLog} alt=""/>
-      <Link to="/" className="nav-link">
-        Home
-      </Link>
-      <Link to="/projects" className="nav-link">
-        Projects
-      </Link>
+        <div className='navLogo'>
+          <img src={navLog} alt=""/>
+          <h3>KEYSTONE CONSTRUCTION <br />
+           <span>BUILDING & RENOVATION</span></h3>
+        </div>
+        <div className='allNavLinks'>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="nav-link">
+            About Us
+          </Link>
+          <Link to="/Services" className='nav-link'>
+            Services
+          </Link>
+          <Link to="/Projects" className='nav-link'>
+             Project
+          </Link>
+          <Link to="/Contact" className='nav-link'>
+             Contact Us
+          </Link>
+        </div>
     </nav>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,40 @@ import React from 'react'
 const Home:React.FC = () => {
   return (
     <div className='homeSection'>
-      <h1>Construction Company Home</h1>
-      <p>Welcome to our construction services website.</p>
+      <div className='welcomeSection'>
+        <div className='heading'>
+          <h1>Transforming <br />Houses into Homes</h1>
+          <h3>Keystone Construction 'Building & Renovation' is dedicated to building lasting relationships with clients 
+            by consistently exceeding expectations through exceptional performance by every team member. We bring years of 
+            industry expertise to every project, ensuring quality, safety, and efficiency from inception to completion.</h3>
+        </div>
+      </div>
+      <div className='aboutSection'>
+          <h2><span id='wSpan'>Welcome</span> to the official page of <br /> <span style={{color:'#f54d23ff'}}>Keystone Construction</span></h2>
+        <div className='aboutPars'>
+          <div>
+            <p>We are dedicated to building the foundations of your future, turning architectural aspirations into 
+            solid, enduring realities.</p>
+          </div>
+          <div>
+            <h3>Uncompromising Craftsmanship:</h3>
+            <p>At Keystone Construction, we don't just build structures; we craft legacies. We use superior materials and 
+               employ highly skilled tradespeople who deliver exceptional quality and durability in every fixture and 
+               finish.</p>
+          </div>
+          <div>
+            <h3>Why Choose Keystone Construction?</h3>
+            <p>The name "Keystone" defines our philosophy: the central stone at the summit of an arch, locking the whole 
+               structure together. This symbolizes the essential role we play in the success of your project. Here’s why 
+               we are the right partner for your next build:</p>
+          </div>
+          <div>
+            <h3>Foundation of Trust and Integrity:</h3>
+            <p>We believe a successful project is built on trust. Our transparent processes and honest communication 
+               ensure you are confident and informed from the initial consultation to project completion.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
