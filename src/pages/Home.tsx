5e8f8ae4ff1d544c1d6fb6f8d28cc6dde25fd778
Mondli <mondli46401@gmail.com>
2025-12-07 18:36:35 +0200

    All pages are working now in terms of responsiveness need to add the email functionality only then done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,15 +62,15 @@ const Home:React.FC = () => {
         <div className='aboutPars'>
           <div>
             <i>We are dedicated to building the foundations of your future, turning architectural aspirations into 
-            solid, enduring realities.</i><br /> <br className='brActive'/>
+            solid, enduring realities.</i><br /> <br className='brActive'/> <br />
             <p><span>Why Choose Keystone Construction?</span><br /> The name "Keystone" defines our philosophy: the central stone at the summit of an arch, locking the whole 
                structure together. This symbolizes the essential role we play in the success of your project. Here's why 
-               we are the right partner for your next build:</p>
+               we are the right partner for your next build:</p> <br />
           </div>          
           <div>
             <p><span>Uncompromising Craftsmanship:</span> <br />At Keystone Construction, we don't just build structures; we craft legacies. We use superior materials and 
                employ highly skilled tradespeople who deliver exceptional quality and durability in every fixture and 
-               finish.</p>
+               finish.</p> <br />
             <p><span>Foundation of Trust and Integrity:</span><br />We believe a successful project is built on trust. Our transparent processes and honest communication 
                ensure you are confident and informed from the initial consultation to project completion.</p>
           </div>
