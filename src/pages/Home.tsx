1fdea783477c3614193ee5da9417cf402aba6acf
Mondli <mondli46401@gmail.com>
2025-12-07 02:50:00 +0200

    Here I just added the responsiveness for the Footer(Mondli Khoza)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,7 +62,7 @@ const Home:React.FC = () => {
         <div className='aboutPars'>
           <div>
             <i>We are dedicated to building the foundations of your future, turning architectural aspirations into 
-            solid, enduring realities.</i><br />
+            solid, enduring realities.</i><br /> <br className='brActive'/>
             <p><span>Why Choose Keystone Construction?</span><br /> The name "Keystone" defines our philosophy: the central stone at the summit of an arch, locking the whole 
                structure together. This symbolizes the essential role we play in the success of your project. Here's why 
                we are the right partner for your next build:</p>
